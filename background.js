3f38dec8333b7804ebef45574d97cd111f72d576
Joris <joris@placette.fr>
2023-08-14 00:32:50 +0200

    axios request , still broken

--- background.js
+++ background.js
@@ -1,4 +1,4 @@
 chrome.action.onClicked.addListener((tab) => {
   // send a message to the content script
-  chrome.tabs.sendMessage(tab.id, { text: 'copy_to_clipboard' });
+  chrome.tabs.sendMessage(tab.id, { text: 'geomannonce-lookup' });
 });
--- content.js
+++ content.js
@@ -1,14 +1,43 @@
+// Include axios for HTTP requests
+// const axios = require('axios');
+const BASE_URL = 'http://127.0.0.1:8000';
+
 chrome.runtime.onMessage.addListener((msg, sender, sendResponse) => {
-    if (msg.text === 'copy_to_clipboard') {
-        
-      let pageHTML = document.documentElement.outerHTML;
-      navigator.clipboard.writeText(pageHTML)
-        .then(() => {
-          console.log('HTML copied to clipboard');
-        })
-        .catch(err => {
-          console.error('Could not copy HTML: ', err);
-        });
-    }
-  });
-  
+  if (msg.text === 'copy_to_clipboard') {
+
+    let pageHTML = document.documentElement.outerHTML;
+    navigator.clipboard.writeText(pageHTML)
+      .then(() => {
+        console.log('HTML copied to clipboard');
+      })
+      .catch(err => {
+        console.error('Could not copy HTML: ', err);
+      });
+  }
+  if (msg.text === 'geomannonce-lookup') {
+
+    let content = document.documentElement.outerHTML;
+    let url = window.location.href;
+    // log url and content as a debug
+    console.log({ url, content });
+
+    // POST request to 127.0.0.1:8000/geomannonce
+    axios.post("http://127.0.0.1:8000/geomannonce/save/", {
+      url: url,
+      content: content
+    }).then(response => {
+      // Parse response
+      let uuid = response.data.uuid;
+      console.log(uuid);
+      let target = `http://127.0.0.1:8000/geomannonce/${uuid}/search/json/`;
+      window.open(target, '_blank');
+      // GET request to the target
+      return axios.get(target);
+    }).then(response => {
+      console.log(url);
+      console.log(response.data);
+    }).catch(error => {
+      console.error("Error:", error);
+    });
+  }
+});
