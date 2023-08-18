296a1954401fc4ddd4d2085b51da163e94c46151
Joris <joris@placette.fr>
2023-08-18 14:09:49 +0200

    handle errors, human readable feedback, cleaner code

--- content.js
+++ content.js
@@ -3,84 +3,79 @@
 // hostnameIsCompatible(hostname) returns true if hostname is compatible with the extension.
 // Compatible hostnames are defined in COMPATIBLE_HOSTNAMES, in env.js
 function hostnameIsSupported(hostname) {
-  // lambda that tests if there is a === or endsWith string match
-  let test = (x) => {
-    if (x === hostname) {
-      return true;
+    // lambda that tests if there is a === or endsWith string match
+    let test = (x) => {
+        if (x === hostname) {
+            return true;
+        }
+        if (hostname.endsWith(x)) {
+            return true;
+        }
+        return false;
     }
-    if (hostname.endsWith(x)) {
-      return true;
-    }
-    return false;
-  }
-  // return true if any of the COMPATIBLE_HOSTNAMES matches
-  return SUPPORTED_HOSTNAMES.some(test);
+    // return true if any of the COMPATIBLE_HOSTNAMES matches
+    return SUPPORTED_HOSTNAMES.some(test);
+}
+
+// wait for the DOM to be interactive or complete
+function domReadyToSend() {
+    return new Promise((resolve, reject) => {
+        if (document.readyState === 'interactive' || document.readyState === 'complete') {
+            resolve();
+        } else {
+            document.addEventListener('readystatechange', function () {
+                if (document.readyState === 'interactive' || document.readyState === 'complete') {
+                    document.removeEventListener('readystatechange', arguments.callee);
+                    resolve();
+                }
+            });
+        }
+    });
+}
+
+function sendDOMToRemoteGeomAnnonce() {
+    let domContent = document.documentElement.outerHTML;
+    let url = window.location.href;
+
+    axios.post(BASE_URL + "/geomannonce/save/", {
+        url: url,
+        content: domContent
+    }).then(response => {
+        let uuid = response.data.uuid;
+        let target = BASE_URL + `/geomannonce/${uuid}/search/`;
+        window.open(target, '_blank');
+        return axios.get(target);
+    }).catch(error => {
+
+        if (error.response) {
+            alert(`Erreur lors du chargement de cette page dans GeomAnnonce. Veuilez contacter le support à contact@webanimus.com avec les informations suivantes :\n\nStatus = ${error.response.status}, Error text = ${error.response.statusText}`);
+        } else {
+            alert("Erreur lors du chargement de cette page dans GeomAnnonce. Veuilez contacter le support à contact@webanimus.com avec les informations suivantes :\n\n" + error.message); // Getting message property of the error object.
+        }
+    });
 }
 
 chrome.runtime.onMessage.addListener((msg, sender, sendResponse) => {
-  //TODO @JorisPLA7: clean this dead code
-  if (msg.text === 'copy_to_clipboard') {
+    if (msg.text === 'geomannonce-lookup') {
 
-    let pageHTML = document.documentElement.outerHTML;
-    navigator.clipboard.writeText(pageHTML)
-      .then(() => {
-        console.log('HTML copied to clipboard');
-      })
-      .catch(err => {
-        console.error('Could not copy HTML: ', err);
-      });
-  }
-  if (msg.text === 'geomannonce-lookup') {
+        let content = document.documentElement.outerHTML;
 
-    let content = document.documentElement.outerHTML;
+        // alert users when they are not on a supported website, and return.
+        if (hostnameIsSupported(window.location.hostname)) {
+            console.log("Compatible hostname recognised: ", window.location.hostname);
+        } else {
+            let alert_message = window.location.hostname + " n'est pas supporté pour le moment.\nVeuillez essayer sur une annonce immobilière sur un site compatible : "
+                + SUPPORTED_HOSTNAMES.join(", ");
+            //TODO @JorisPLA7: replace alert by a cleaner popup (to make in figma 1st)
+            alert(alert_message);
+            return;
+        }
 
-    // alert users when they are not on a supported website, and return.
-    if (hostnameIsSupported(window.location.hostname)) {
-      console.log("Compatible hostname recognised: ", window.location.hostname);
-    } else {
-      let alert_message = window.location.hostname + " n'est pas supporté pour le moment.\nVeuillez essayer sur une annonce immobilière sur un site compatible : "
-        + SUPPORTED_HOSTNAMES.join(", ");
-      //TODO @JorisPLA7: replace alert by a cleaner popup (to make in figma 1st)
-      alert(alert_message);
-      return;
+        async function shipPayload() {
+            await domReadyToSend();
+            sendDOMToRemoteGeomAnnonce();
+        }
+        shipPayload();
+
     }
-
-    // wait for the DOM to be loaded
-    function sendDOMToRemoteGeomAnnonce() {
-      // Your code to copy the DOM content
-      let domContent = document.documentElement.outerHTML;
-      console.log(domContent);
-    }
-
-    // // ie if document.readyState is not loading, send the DOM to the remote server
-    // // from interactive and beyond, the DOM html is complete, but images, stylesheets, and subframes may still be loading.
-    // if (document.readyState === 'interactive' || document.readyState === 'complete') {
-    //     sendDOMToRemoteGeomAnnonce();
-    //   alert("DOM is ready already");
-    // } else {
-
-    //   document.addEventListener('readystatechange', function () {
-    //     if (document.readyState === 'interactive' || document.readyState === 'complete') {
-    //       sendDOMToRemoteGeomAnnonce();
-    //       alert("DOM is ready now");
-    //     }
-    //   });
-    // }
-
-
-    // POST request to 127.0.0.1:8000/geomannonce
-    axios.post(BASE_URL + "/geomannonce/save/", {
-      url: url,
-      content: content
-    }).then(response => {
-      // Parse response
-      let uuid = response.data.uuid;
-      let target = BASE_URL + `/geomannonce/${uuid}/search/`;
-      window.open(target, '_blank');
-      // GET request to the target
-      return axios.get(target);
-    }).catch(error => {
-      console.error("Error:", error);
-    });
-  }
 });
