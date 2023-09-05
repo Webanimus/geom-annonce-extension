7d178b52873a9a579f9630daba431c3fbc6e8b3e
MercierCorentin <corentin@cmercier.fr>
2023-09-05 22:03:28 +0200

    Add www to avoid 301 redirect

--- env.js
+++ env.js
@@ -1,4 +1,4 @@
-const BASE_URL = 'https://app.geomservices.com';
+const BASE_URL = 'https://www.app.geomservices.com';
 
 //TODO @JorisPLA7 rendre plus précis, cibler uniquement les pages d'annonces immobilières.
 const SUPPORTED_HOSTNAMES = [
