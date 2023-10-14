3b7db1f23854a1cd2cdebd2a472a04d7282a6133
Joris <joris@placette.fr>
2023-10-14 15:55:24 +0200

    new popup

--- popup.js
+++ popup.js
@@ -1,9 +1,11 @@
 document.addEventListener('DOMContentLoaded', function() {
-    // document.getElementById('btn').addEventListener('click', function() {
-    //     chrome.scripting.executeScript({
-    //         target: { tabId: tab.id },
-    //         files: ['content.js']
-    //     });
-    // }, false);
-    console.log('popup.js loaded');
-});
+    // Query for the active tab in the current window
+    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+      // tabs[0] is the first (and only) tab returned by the query
+      const currentTab = tabs[0];
+  
+      // Send a message to the content script running in the current tab
+      chrome.tabs.sendMessage(currentTab.id, { text: 'geomannonce-lookup' });
+    });
+  });
+  
