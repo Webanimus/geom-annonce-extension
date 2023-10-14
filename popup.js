730b75a16eb70cf5052ca67e98c35f5ab44fd331
Joris <joris@placette.fr>
2023-10-14 14:24:40 +0200

    clean old files

---
+++ popup.js
@@ -0,0 +1,9 @@
+document.addEventListener('DOMContentLoaded', function() {
+    // document.getElementById('btn').addEventListener('click', function() {
+    //     chrome.scripting.executeScript({
+    //         target: { tabId: tab.id },
+    //         files: ['content.js']
+    //     });
+    // }, false);
+    console.log('popup.js loaded');
+});
